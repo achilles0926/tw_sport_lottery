68501058f55c7c1f966b068b25a7b05f9b2602de
Achilles <61537822+achilles0926@users.noreply.github.com>
2023-07-13 08:53:45 +0800

    [fix]parse_resp_by_sport放回try, main 新增處理爬無資料之處理

--- mlb_odds_crawler.py
+++ mlb_odds_crawler.py
@@ -44,22 +44,29 @@ def parse_resp_by_sport(sport_code=443):
     data = resp.json()
     output = []
     for game_dict in data:
-        idx = game_dict['id']
-        sport = game_dict['ln'][0]
-        team_away = game_dict['atn']
-        team_home = game_dict['htn']
-        odd_info = game_dict['ms'][0]['cs'][0]
-        odd_away = odd_info[0]['o']
-        odd_home = odd_info[1]['o']
-        output.append({
-            'idx': idx,
-            'sport': sport,
-            'team_away': team_away,
-            'team_home': team_home,
-            'odd_away': odd_away,
-            'odd_home': odd_home,
-            'etl_dttm': etl_dttm
-        })
+        try:
+            idx = game_dict['id']
+            sport = game_dict['ln'][0]
+            team_away = game_dict['atn']
+            team_home = game_dict['htn']
+            odd_info = game_dict['ms'][0]['cs'][0]
+            odd_away = odd_info[0]['o']
+            odd_home = odd_info[1]['o']
+            output.append({
+                'idx': idx,
+                'sport': sport,
+                'team_away': team_away,
+                'team_home': team_home,
+                'odd_away': odd_away,
+                'odd_home': odd_home,
+                'etl_dttm': etl_dttm
+            })
+        except IndexError:
+            print(sport)
+            continue
+        except KeyError:
+            print(sport)
+            continue
     df_output = pd.DataFrame(output)
     return df_output
 
@@ -68,6 +75,12 @@ def main():
     try:
         # crawler
         df_output = parse_resp_by_sport(sport_code=443)
+        
+        if len(df_output) == 0:
+            msg = '本時間點爬無賠率資料'
+            send_msg_to_LINE(msg)
+            reutrn 
+        
         df_output['name_away'] = df_output['team_away'].apply(lambda x: x[1])
         df_output['name_home'] = df_output['team_home'].apply(lambda x: x[1])
         df_output = df_output.drop(['team_away', 'team_home'], axis=1)
